72ff91ad248d3b56e80bab81fcaac7efd5b3d9c4
tmac9494 <trentmcdole94@gmail.com>
2020-03-04 12:54:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ class Draggable {
 	constructor(container, target) {
 		this.container = container;
 		this.target = target;
-		this.initializeListeners();
 		this.mousedown = false;
 		this.dragging = false;
 		this.start = null;
@@ -10,44 +9,43 @@ class Draggable {
 		this.max = (window.innerWidth - (window.innerWidth * .6)) - this.target.clientWidth;
 		this.moving = false;
 		this.moveFrame = null;
-		this.handleScroll();
-		console.log(this.max)
-	}
-	initializeListeners = () => {
 		this.container.style.userSelect = 'none';
-		this.container.addEventListener('mousedown', this.handleMouseDown);
-		this.container.addEventListener('touchstart', this.handleMouseDown);
-		this.container.addEventListener('mouseup', this.handleMouseUp);
-		this.container.addEventListener('touchend', this.handleMouseUp);
-		this.container.addEventListener('mousemove', this.handleMouseMove);
-		this.container.addEventListener('touchmove', this.handleMouseMove);
-		this.container.addEventListener('mouseleave', this.handleMouseLeave);
-		this.container.addEventListener('mouseenter', this.handleMouseEnter);
-		window.addEventListener('scroll', this.handleScroll)
+		window.addEventListener('scroll', this.handleScroll.bind(this), {passive: true});
+    ['handleScroll', 'startMoving', 'stopMoving', 'move']
+    .forEach(fName =>  this[fName].bind(this) );
+		this.container.addEventListener('mousedown', this.handleMouseDown.bind(this), {passive: true});
+		this.container.addEventListener('touchstart', this.handleMouseDown.bind(this), {passive: true});
+		this.container.addEventListener('mouseup', this.handleMouseUp.bind(this));
+		this.container.addEventListener('touchend', this.handleMouseUp.bind(this));
+		this.container.addEventListener('mousemove', this.handleMouseMove.bind(this), {passive: true});
+		this.container.addEventListener('touchmove', this.handleMouseMove.bind(this), {passive: true});
+		this.container.addEventListener('mouseleave', this.handleMouseLeave.bind(this));
+		this.container.addEventListener('mouseenter', this.handleMouseEnter.bind(this));
+		this.handleScroll();
 	}
-	handleScroll = e => {
+	handleScroll(e) {
 		const inView = isElementInView(this.container);
 		if (inView && !this.moving) this.startMoving()
 		else if (!inView && this.moving) this.stopMoving();
 	}
-	startMoving = () => {
+	startMoving() {
 		this.moving = true;
-		this.moveFrame = requestAnimationFrame(this.move);
+		this.moveFrame = requestAnimationFrame(() => this.move(this));
 	}
-	stopMoving = () => {
+	stopMoving() {
 		this.moving = false;
 		cancelAnimationFrame(this.moveFrame);
 		this.moveFrame = null;
 	}
-	move = () => {
-    if (!this.dragging) this.dragging = true;
-		if (this.atEnd) this.stopMoving()
+	move(thisRef) {
+    if (!thisRef.dragging) { thisRef.dragging = true; }
+		if (thisRef.atEnd) { thisRef.stopMoving() }
 		else {
-			this.handlePosition(-.5);
-			this.moveFrame = requestAnimationFrame(this.move);
+			thisRef.handlePosition(-.5);
+			thisRef.moveFrame = requestAnimationFrame(() => thisRef.move(thisRef));
 		}
 	}
-	handleMouseMove = e => {
+	handleMouseMove(e) {
 		if (this.mousedown) {
 			const clientX = e.touches ? e.touches[0].clientX : e.clientX;
 			const distance = clientX - this.start;
@@ -55,23 +53,25 @@ class Draggable {
 			this.start = clientX;
 		}
 	}
-	handleMouseDown = e => {
+	handleMouseDown(e) {
 		this.mousedown = true;
 		this.start = e.touches ? e.touches[0].clientX : e.clientX;
 		if (this.moving) this.stopMoving();
 	}
-	handleMouseUp = e => {
+	handleMouseUp(e) {
 		this.mousedown = false;
 		this.start = null;
     this.dragging = false;
 	}
-  handleMouseEnter = e => this.moving ? this.stopMoving() : null;
-  handleMouseLeave = e => {
+  handleMouseEnter(e) {
+		if (this.moving) this.stopMoving();
+	}
+  handleMouseLeave(e) {
     if (!this.moving) this.startMoving();
     this.mousedown = false;
     this.dragging = false;
   }
-	handlePosition = distance => {
+	handlePosition(distance) {
 		let update = this.xPos + distance;
 		// at start check
 		if (distance >= 0 && this.xPos >= 0) update = 0;
@@ -79,7 +79,7 @@ class Draggable {
 		if (distance < 0 && this.xPos <= this.max) update = this.max;
 		if (update !== this.xPos) this.handleRender(update);
 	}
-	handleRender = (update) => {
+	handleRender(update) {
 		this.xPos = update;
 		requestAnimationFrame(() => {
 			this.target.style.transform = 'translate(' + this.xPos + 'px, -50%)';
